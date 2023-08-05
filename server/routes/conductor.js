903b7681ab76d8e8964df5cc85f3c95ccf7eeace
JoSeLoDiAz <josediazd40z@gmail.com>
2023-08-04 22:44:39 -0500

    ultimo Round asi sube

--- server/routes/conductor.js
+++ server/routes/conductor.js
@@ -27,7 +27,7 @@ router.post('/', [
   check("direccion").isLength({ max: 255 }).withMessage("La dirección debe tener máximo 255 caracteres"),
   check("clase_pase", "La clase de pase es un campo requerido").not().isEmpty(),
   check("clase_pase")
-    .isIn(["B2", "C1"])
+    .isIn(["B2","b2","c1", "C1"])
     .withMessage("El pase debe ser para operar servicio público y estar en las categorías B2 o C1"),
   check("vigencia_pase", "La vigencia de pase es un campo requerido").not().isEmpty(),
   check("vigencia_pase").isDate().withMessage("La vigencia de pase debe ser una fecha válida"), validarResultados
